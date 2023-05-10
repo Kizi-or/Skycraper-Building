2f7174aa22a011e0b6f35b61c0a15594f644935e
Sebastian Kizior <kiziorsebastian@gmail.com>
2023-05-10 11:58:36 +0200

    prototype

--- Skycraper-Building/Assets/Scripts/LevelScripts/LineScript.cs
+++ Skycraper-Building/Assets/Scripts/LevelScripts/LineScript.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class LineScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float speed = 5;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.forward * speed * Time.deltaTime);
     }
 }
